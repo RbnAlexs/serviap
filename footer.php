530c601dde5096b62328464421f046649165b255
Ruben Sanchez <rubensanchezcolin@gmail.com>
2022-02-01 12:35:30 -0600

    Versión 1.0 Child Theme, funciones basicas heredadas

---
+++ footer.php
@@ -0,0 +1,212 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Orange
+ */
+ 
+wp_footer(); ?>
+<!-- [footer] -->
+  
+<footer class="footer padding-top-lmedium">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5">
+          <div class="row">
+            <div class="col-sm-6 col-md-8">
+              <div class="footer-block footer-block-contacts">
+                <p class="footer-title h4">Contacts</p>
+                <ul>
+                  <li class="tooltipster tooltipster-dflt" title="">
+                    <i class='fa fa-phone'></i>  
+                    <a href="callto:15127661720" data-toggle="modal" data-target=".map-modal">U.S and Canada +1 (512) 766 1720</a>
+                  </li>
+                  <li>
+                     <i class='fa fa-phone'></i>  
+                    <a href="callto:5553792130" data-toggle="modal" data-target=".map-modal">Mexico +52 (55) 5379 2130</a>
+                  </li>
+                 
+                  <li>
+                    <i class='fa fa-envelope-o'></i>  
+                    <a href="mailto: sales@serviapgroup.com"><span class="__cf_email__" data-cfemail="4c29342d213c20290c29342d213c2029622f2321">sales@serviapgroup.com</span></a>
+                  </li>
+					
+					<li>
+                    <i class='fa fa-angle-double-right'></i>  
+                    <a href="https://www.serviapgroup.com/contact/">United States</a>
+                  </li>
+					
+					<li>
+                    <i class='fa fa-angle-double-right'></i>  
+                    <a href="https://www.serviapgroup.com/contact/">Mexico</a>
+                  </li>
+					
+					
+					<li>
+                    <i class='fa fa-angle-double-right'></i>  
+                    <a href="https://www.serviapgroup.com/contact/">Argentina</a>
+                  </li>
+					
+					<li>
+                    <i class='fa fa-angle-double-right'></i>  
+                    <a href="https://www.serviapgroup.com/contact/">Chile</a>
+                  </li>
+					
+					<li>
+                    <i class='fa fa-angle-double-right'></i>  
+                    <a href="https://www.serviapgroup.com/contact/">Brazil</a>
+                  </li>
+					
+					<li>
+                    <i class='fa fa-angle-double-right'></i>  
+                    <a href="https://www.serviapgroup.com/contact/">Peru</a>
+                  </li>
+					
+					<li>
+                    <i class='fa fa-angle-double-right'></i>  
+                    <a href="https://www.serviapgroup.com/contact/">Colombia</a>
+                  </li>
+					
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+              <div class="footer-block">
+                <p class="footer-title h4">Content</p>
+                <ul>
+                  <li><a href="https://www.serviapgroup.com/peo-professional-employer-organization/"><i class="fa fa-angle-double-right"></i> International PEO</a></li>
+                  <li><a href="https://www.serviapgroup.com/peo-professional-employer-organization-services/"><i class="fa fa-angle-double-right"></i> Countries We Cover</a></li>
+                  <li><a href="https://www.serviapgroup.com/blog/"><i class="fa fa-angle-double-right"></i> Blog</a></li>
+                  <li><a href="https://www.serviapgroup.com/content/"><i class="fa fa-angle-double-right"></i> Content</a></li>
+                  <li><a href="https://www.serviapgroup.com/peo-employer-of-record-company/"><i class="fa fa-angle-double-right"></i> About Us</a></li>
+					<li><a href="http://careers.serviapgroup.com/"><i class="fa fa-angle-double-right"></i> Careers</a></li>
+					
+					<li><a href="https://www.serviapgroup.com/partners/"><i class="fa fa-angle-double-right"></i> Partners</a></li>
+					
+					 <li><a href="https://www.serviapgroup.com/contact/"><i class="fa fa-angle-double-right"></i> Contact</a></li>
+					
+						 <li><a href="https://www.serviapgroup.com/events/"><i class="fa fa-angle-double-right"></i> Events</a></li>
+					
+					 <li><a href="https://www.serviapgroup.com/request-a-quote-serviap/"><i class="fa fa-angle-double-right"></i> Request a Proposal</a></li>
+					
+				
+					
+                </ul>
+              </div>
+            </div>
+            <div class="col-xs-12">
+              <div class="row">
+               
+                <div class="col-sm-6 col-md-12">
+                  <div class="footer-block footer-block-logo-text">
+					  <!--
+					  <p class="footer-text-logo "> 
+<a href="https://www.serviapgroup.com/terms-and-conditions/">Terms and Condition</a> | 
+	   <a href="https://www.serviapgroup.com/privacy-policy/">Privacy Policy</a></p>
+	  -->
+					  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  
+  
+  
+  
+        <div class="col-sm-6 col-md-4">
+          <div class="footer-block footer-block-news">
+            <p class="footer-title h4">Recent news</p>
+            <ul>
+				
+				
+				<li>
+                <a href="https://www.serviapgroup.com/blog/global-outsoourcing/">Domestic vs Global Outsourcing:</a><br>
+                <em class="block date color-555">04 Oct, 2021</em>
+              </li>
+				
+              <li>
+                <a href="https://www.serviapgroup.com/blog/hiring-employees/">
+					The Costs of Hiring a New Employee</a><br>
+                <em class="block date color-555">04 Oct, 2021</em>
+              </li>
+              <li>
+                <a href="https://www.serviapgroup.com/blog/workforce-diverse/">How to Build a Diverse Workforce</a><br>
+                <em class="block date color-555">27 Sep, 2021</em>
+              </li>
+			
+            
+            </ul>
+          </div>
+        </div>
+  
+        <div class="col-sm-6 col-md-3">
+          <div class="footer-block footer-block-counter">
+			  <p class="footer-title h4">Connect with Serviap</p>
+            
+          </div>
+          <div class="footer-block footer-block-social footer-block-social-desc social-icons social-smedium social-rounded">
+            <div class="row">
+              <div class="col-xs-12 col-lg-6">
+				  
+				  
+				  
+			<ul class="social-icon-four" style="text-align:center">
+                                <li><a href="https://www.facebook.com/Serviapgroup/?ref=br_rs" target="_blank"><span class="fa fa-facebook"></span></a>    
+									
+<a href="https://www.linkedin.com/company/serviap/?originalSubdomain=mx" target="_blank"><span class="fa fa-linkedin"></span></a>   
+
+									<a href="https://twitter.com/SERVIAPgroup" target="_blank"><span class="fa fa-twitter"></span></a>
+					  
+					  </li>
+                               
+					  
+                            </ul>
+				  
+				  
+				  
+
+              </div>
+               
+               
+            </div>
+          </div>
+          <div class="footer-block footer-block-devices">
+			  <div class="wow fadeInUp device-big-image">
+              <picture>
+                <source srcset="https://www.serviapgroup.com/wp-content/uploads/2020/05/logo-footer.png" type="image/webp">
+                <img src="https://www.serviapgroup.com/wp-content/uploads/2020/05/logo-footer.png" alt="iPhone">
+              </picture>
+            </div>
+            
+         
+          </div>
+        </div>
+  
+      </div>
+    </div>
+  </footer>
+  
+  <!-- END OF [footer] -->
+<!-- END OF [footer] -->
+  	
+    <p style="background-color:#1A72AA; width:100%; text-align:center;">
+        <a style="color:#fff;" href="https://www.serviapgroup.com/terms-and-conditions/">Terms and Condition | </a>
+        <a style="color:#fff;" href="https://www.serviapgroup.com/privacy-policy/">Privacy Policy</a>
+    </p>
+    <div class="copyright p-t p-b">
+        <div class="container">
+		<p class="copyright-text text-center">Copyright © 2022 Serviapgroup. All Rights Reserved.
+    </div>
+  </div>
+    <script src="//code.tidio.co/gyj5ocbc2unznxoayl8l8vozfkbmx9us.js" async></script>
+    <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left">
+        <span><i class="fa fa-angle-up"></i></span>
+    </a>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Orange
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WR49ZN5');</script>
+	<!-- End Google Tag Manager -->
+</head>
+<body <?php body_class(); ?>>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WR49ZN5');</script>
+<!-- End Google Tag Manager -->
+<?php orange_header();?>
